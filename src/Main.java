164d39aac88687e01f5151d62e2f943866306799
Luiz Silva <luiz.2019206583@unicap.br>
2024-10-11 21:42:44 +0000

    FirstCommit - InicialThink

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        int[][] initialState = {
+            {1, 2, 3},
+            {4, 0, 5},
+            {6, 7, 8}
+        };
+
+        int[][] goalState = {
+            {1, 2, 3},
+            {4, 5, 0},
+            {6, 7, 8}
+        };
+    };
+};
