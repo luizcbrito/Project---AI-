d4b1163cc0d0a3a3aa290447aea24e13b91d0c57
Luiz Silva <luiz.2019206583@unicap.br>
2024-10-11 21:49:15 +0000

    FinalCommit - ImplementMain

--- src/Main.java
+++ src/Main.java
@@ -16,5 +16,30 @@ public class Main {
         PuzzleState goal = new PuzzleState(goalState);
         Node root = new Node(initial, null, 0, null);
 
-    };
-};
+        IDDFS iddfs = new IDDFS();
+
+        // Usando geração de filhos com cópia
+        long startTimeCopy = System.nanoTime();
+        Node resultCopy = iddfs.iddfsWithCopy(root, goal, 20);
+        long endTimeCopy = System.nanoTime();
+
+        if (resultCopy != null) {
+            System.out.println("Solução encontrada com cópia!");
+        } else {
+            System.out.println("Solução não encontrada com cópia.");
+        }
+        System.out.println("Tempo de execução com cópia: " + (endTimeCopy - startTimeCopy) + " ns");
+
+        // Usando geração de filhos com modificação direta
+        long startTimeMod = System.nanoTime();
+        Node resultMod = iddfs.iddfsWithModification(root, goal, 20);
+        long endTimeMod = System.nanoTime();
+
+        if (resultMod != null) {
+            System.out.println("Solução encontrada com modificação direta!");
+        } else {
+            System.out.println("Solução não encontrada com modificação direta.");
+        }
+        System.out.println("Tempo de execução com modificação direta: " + (endTimeMod - startTimeMod) + " ns");
+    }
+}
